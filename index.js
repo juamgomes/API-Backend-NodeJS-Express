df612164579878007da963a0f67d18644df70a29
Juan Gomes <89032763+juamgomes@users.noreply.github.com>
2023-02-07 11:35:40 -0400

    Listagem dos endpoints e criação de endpoints de leitura(individual e todos)

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const express = require('express');
+const { send } = require('express/lib/response');
 const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.send('Hello World');
 });
+
+/*
+Lista de Endpoints da aplicação CRUD de mensagens
+CRUD: Create, Read (Single & All), Update and Delete
+CRUD: Criar, Ler (Individual e Tudo), Atualizar e Removeer
+- [GET] /Mensagens - Retorna a lista de mensagens
+- [GET] /Mensagens/{ID} - Retorna uma unica mensagem pelo ID
+- [POST] /Mensagens - Cria novas mensagens 
+- [PUT] /Mensagens{id} - Atualiza uma mensagem pelo ID
+- [DELETE] /Mensagens/{id} - Remover uma mensagem pelo ID
+*/
+
+const mensagens = [
+    "Primeira mensagem",
+    "Segunda mensagem"
+]
+
+// - [GET] /Mensagens - Retorna a lista de mensagens
+app.get('/mensagens', (req, res) => {
+    res.send(mensagens);
+});
+    
+app.get('/mensagens/:id', (req, res) => {
+    const id = req.params.id - 1;
+
+    const mensagem = mensagens[id];
+
+    res.send(mensagem);
+})
+
+
 
 app.listen(port, () => {
     console.info(`App rodando em http://localhost:${port}`);
